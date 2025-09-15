3335d1e5223851621091541626ffeb172fb2cd67
eliezerchavez <eliezer.chavez@gmail.com>
2025-09-15 15:46:08 -0400

    new animation effects and duration

--- js/app.js
+++ js/app.js
@@ -1,18 +1,3 @@
-/** 
- * 
-*/
 
-const STYLE = {
-    "COL-01": { logo: { bg: "#FFFFFF", glow: "#002E6D", src: "img/col-01.svg" }, button: { fg: "#002E6D" } },
-    "COL-02": { logo: { bg: "#002E6D", glow: "#FFFFFF", src: "img/col-02.svg" }, button: { fg: "#002E6D" } },
-    "GRS-01": { logo: { bg: "#FFFFFF", glow: "#1D252C", src: "img/grs-01.svg" }, button: { fg: "#1D252C" } },
-    "GRS-02": { logo: { bg: "#1D252C", glow: "#FFFFFF", src: "img/grs-02.svg" }, button: { fg: "#1D252C" } },
-    "NEG-01": { logo: { bg: "#1D252C", glow: "#FFFFFF", src: "img/neg-01.svg" }, button: { fg: "#1D252C" } },
-    "OUT-01": { logo: { bg: "#FFFFFF", glow: "#1D252C", src: "img/out-01.svg" }, button: { fg: "#1D252C" } },
-    "POS-01": { logo: { bg: "#FFFFFF", glow: "#FFFFFF", src: "img/pos-01.svg" }, button: { fg: "#002E6D" } },
-    "POS-02": { logo: { bg: "#FFFFFF", glow: "#1D252C", src: "img/pos-02.svg" }, button: { fg: "#00C4B3" } }
-};
-
-const qs = new URLSearchParams(window.location.search); // Grab the query string from the current URL
 
 
